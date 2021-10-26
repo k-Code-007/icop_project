fb06203c017a3732f70fbee5fd6e0c11fbf0cf64
KateM3d <ekaterina8medvedeva@gmail.com>
2021-10-25 20:43:16 -0400

    added slider switch added automatic slider switch

--- assets/scripts/slider.js
+++ assets/scripts/slider.js
@@ -1,7 +1,7 @@
 const back = document.querySelector('#back');
 const next = document.querySelector('#next');
-const videos = ["./assets/video/videoSergey.mp4", "assets/video/videoSergey.mp4", "assets/video/videoSergey.mp4", "assets/video/videoSergey.mp4", "assets/video/videoSergey.mp4"]
-
+const videos = ["./assets/video/1.mp4", "assets/video/2.mp4", "assets/video/3.mp4", "assets/video/4.mp4", "assets/video/videoSergey.mp4"]
+const vd1 = document.querySelector('#myVideo');
 
 let i = 0;
 
@@ -20,3 +20,11 @@ back.addEventListener('click', () => {
     }
     document.querySelector('#myVideo').src = videos[i];
 })
+
+vd1.onended = function() {
+    i++;
+    if (i > videos.length - 1) {
+        i = 0;
+    }
+    document.querySelector('#myVideo').src = videos[i];
+}
